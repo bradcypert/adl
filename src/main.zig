fcb4ffbd1e0dcd5e2175072fd1ffbd1501b89da7
johan0A <arnaudchesse@outlook.com>
2025-05-24 17:48:45 +0200

    refactor main.zig

--- src/main.zig
+++ src/main.zig
@@ -10,43 +10,39 @@ fn compareStrings(_: void, lhs: []const u8, rhs: []const u8) bool {
     return std.mem.order(u8, lhs, rhs).compare(std.math.CompareOperator.lt);
 }
 
-fn readFileIfExists(allocator: std.mem.Allocator, filepath: []const u8) ![]u8 {
-    const template_file = try std.fs.cwd().openFile(filepath, .{});
+fn readFileIfExists(allocator: std.mem.Allocator, filepath: []const u8) !?[]u8 {
+    const template_file = std.fs.cwd().openFile(filepath, .{}) catch return null;
     defer template_file.close();
-
-    const file_contents = try template_file.readToEndAlloc(allocator, std.math.maxInt(usize));
-    return file_contents;
+    return try template_file.readToEndAlloc(allocator, std.math.maxInt(usize));
 }
 
 fn rebuildReadme(arena_alloc: std.mem.Allocator) !void {
-    const template_contents = readFileIfExists(arena_alloc, "./adr/templates/template_readme.md") catch readme_contents;
+    const template_contents = try readFileIfExists(arena_alloc, "./adr/templates/template_readme.md") orelse readme_contents;
 
     const date = try Datetime.now().formatHttp(arena_alloc);
     const output = try std.mem.replaceOwned(u8, arena_alloc, template_contents, "{{timestamp}}", date);
     const files = try getAllFilesInADRDir(arena_alloc);
-    var formatted_files = std.ArrayList([]const u8).init(arena_alloc);
+    var formatted_files = std.ArrayList(u8).init(arena_alloc);
 
     std.mem.sort([]const u8, files.items, {}, compareStrings);
 
     for (files.items) |str| {
-        const new_str = try std.fmt.allocPrint(arena_alloc, " - [{s}](./{s})", .{ str, str });
-        try formatted_files.append(new_str);
+        const new_str = try std.fmt.allocPrint(arena_alloc, " - [{s}](./{s})\n", .{ str, str });
+        try formatted_files.appendSlice(new_str);
     }
 
-    const replacement = try std.mem.join(arena_alloc, "\n", formatted_files.items);
-    const withContents = try std.mem.replaceOwned(u8, arena_alloc, output, "{{contents}}", replacement);
+    const with_contents = try std.mem.replaceOwned(u8, arena_alloc, output, "{{contents}}", formatted_files.items);
 
     const f = try std.fs.cwd().createFile("./adr/README.md", .{});
     defer f.close();
-
-    _ = try f.write(withContents);
+    try f.writeAll(with_contents);
 }
 
 fn generateADR(arena_alloc: std.mem.Allocator, n: u64, name: []u8) !void {
     const padded_nums = try std.fmt.allocPrint(arena_alloc, "{:0>5}", .{n});
     const heading = try std.fmt.allocPrint(arena_alloc, "{s} - {s}", .{ padded_nums, name });
 
-    const template_contents = readFileIfExists(arena_alloc, "./adr/templates/template_adr.md") catch adr_contents;
+    const template_contents = try readFileIfExists(arena_alloc, "./adr/templates/template_adr.md") orelse adr_contents;
     const contents = try std.mem.replaceOwned(u8, arena_alloc, template_contents, "{{name}}", heading);
 
     const safe_name = try arena_alloc.dupe(u8, name);
@@ -59,19 +55,16 @@ fn generateADR(arena_alloc: std.mem.Allocator, n: u64, name: []u8) !void {
 
     const f = try std.fs.cwd().createFile(file_name, .{});
     defer f.close();
-
     try f.writeAll(contents);
 }
 
 fn establishCoreFiles() !void {
     const cwd = std.fs.cwd();
-
     try cwd.makePath("./adr/assets");
     try cwd.makePath("./adr/templates");
 
     const f = try cwd.createFile("./adr/templates/README.md", .{});
     defer f.close();
-
     try f.writeAll(template_readme_contents);
 }
 
@@ -81,13 +74,13 @@ fn getAllFilesInADRDir(allocator: std.mem.Allocator) !std.ArrayList([]const u8) 
 
     var file_list = std.ArrayList([]const u8).init(allocator);
 
-    var dirIterator = dir.iterate();
-    while (try dirIterator.next()) |dir_content| {
-        if (!std.mem.eql(u8, dir_content.name, "README.md") and
-            !std.mem.eql(u8, dir_content.name, "assets") and
-            !std.mem.eql(u8, dir_content.name, "templates"))
+    var dir_iterator = dir.iterate();
+    while (try dir_iterator.next()) |entry| {
+        if (!std.mem.eql(u8, entry.name, "README.md") and
+            !std.mem.eql(u8, entry.name, "assets") and
+            !std.mem.eql(u8, entry.name, "templates"))
         {
-            const file_name = try allocator.dupe(u8, dir_content.name);
+            const file_name = try allocator.dupe(u8, entry.name);
             try file_list.append(file_name);
         }
     }
@@ -105,16 +98,14 @@ pub fn main() !void {
     const arena_alloc = arena.allocator();
 
     var args = try std.process.argsAlloc(arena_alloc);
-
     const stdout = std.io.getStdOut().writer();
     const stderr = std.io.getStdErr().writer();
 
     const action = if (args.len > 1) args[1] else "";
-
     if (std.mem.eql(u8, action, "create")) {
         const name: []u8 = if (args.len > 2) try std.mem.join(arena_alloc, " ", args[2..]) else &.{};
         if (name.len == 0) {
-            try stderr.print("No name supplied for the ADR. Command should be: adl create Name of ADR here\n", .{});
+            try stderr.print("No name supplied for the ADR.\nCommand should be: `adl create <Name of ADR here>`\n", .{});
             return;
         }
 
@@ -126,6 +117,6 @@ pub fn main() !void {
         try establishCoreFiles();
         try rebuildReadme(arena_alloc);
     } else {
-        _ = stdout.write(help_contents) catch @panic("Unable to write help contents");
+        stdout.writeAll(help_contents) catch @panic("Unable to write help contents");
     }
 }
